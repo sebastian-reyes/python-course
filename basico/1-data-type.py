125372dc2ebf59ae19f7172ed7893e5243d4d842
sebastian-reyes <sebastianreyes2105@gmail.com>
2025-02-23 03:04:34 -0500

    basic: data types

---
+++ basico/1-data-type.py
@@ -0,0 +1,26 @@
+# string
+name = "sebastian"
+last_name = "reyes"
+full_name = name + " " + last_name
+print(name)
+print(type(name))
+print("hello " + full_name + " :D")
+
+#integer
+age = 22
+age+=1 # age = age + 1
+print(age)
+print(type(age))
+print("you are " + str(age) + " years old")
+
+#float
+height = 1.75
+print(height)
+print(type(height))
+print("you are " + str(height) + " meters tall")
+
+#boolean
+is_student = True
+print(is_student)
+print(type(is_student))
+print("are you a student? " + str(is_student))
